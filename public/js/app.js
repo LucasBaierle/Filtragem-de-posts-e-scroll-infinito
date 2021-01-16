6e7f84908cbcd2123a9b7aac1f13b4f080a61056
LucasBaierle <lucasbayer24@gmail.com>
2021-01-16 11:10:05 -0300

    add filter

--- public/js/app.js
+++ public/js/app.js
@@ -5,62 +5,74 @@ const filterInput = document.querySelector("#filter")
 let page = 1
 
 const getPosts = async () => {
-  const response = await fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`)
+  const response = await
+    fetch(`https://jsonplaceholder.typicode.com/posts?_limit=5&_page=${page}`)
   return response.json()
 }
 
-
-const addPostsIntoDOM = async () => {
-  const posts = await getPosts()
-  const postsTemplate = posts.map(({ id, title, body }) => `
+const generetePostTemplate = posts => posts.map(({ id, title, body }) => `
     <div class="post">
       <div class="number">${id}</div>
       <div class="post-info">
         <h2 class="post-title">${title}</h2>
         <p class="post-body">${body}</p>
       </div>
-    </div> `).join('')
+    </div> 
+`).join('')
+
+const addPostsIntoDOM = async () => {
+  const posts = await getPosts()
+  const postsTemplate = generetePostTemplate(posts)
+
 
   postsContainer.innerHTML += postsTemplate
 }
-addPostsIntoDOM()
 
 const getNextPosts = () => {
-  page++;
-  console.log(page)
-  addPostsIntoDOM()
+  setTimeout(() => {
+    page++;
+    addPostsIntoDOM()
+  }, 300)
 }
 const removeLoader = () => {
   setTimeout(() => {
     loaderContainer.classList.remove("show")
     getNextPosts()
   }, 1000)
-
 }
 const showLoader = () => {
   loaderContainer.classList.add("show")
   removeLoader()
 }
-
-window.addEventListener("scroll", () => {
+const handleScrollToPageBottom = () => {
   const { clientHeight, scrollHeight, scrollTop } = document.documentElement
-  const isPageBottomAlmostReached = scrollTop + clientHeight >= scrollHeight - 10
+  const isPageBottomAlmostReached = scrollTop + clientHeight
+    >= scrollHeight - 10
   if (isPageBottomAlmostReached) {
     showLoader()
   }
-})
+}
 
-filterInput.addEventListener("input", event => {
+const showPostIfMatchInputValue = inputValue => post => {
+  const postTitle = post.querySelector(".post-title").textContent.toLowerCase()
+  const postBody = post.querySelector(".post-body").textContent.toLowerCase()
+  const postContainsInputValue = postTitle.includes(inputValue)
+    || postBody.includes(inputValue)
+
+  if (postContainsInputValue) {
+    post.style.display = "flex";
+    return
+  }
+  post.style.display = "none"
+
+}
+
+const handleInputValue = event => {
   const inputValue = event.target.value.toLowerCase()
   const posts = document.querySelectorAll(".post")
+  posts.forEach(showPostIfMatchInputValue(inputValue))
+}
 
-  posts.forEach(post => {
-    const postTitle = post.querySelector(".post-title").textContent.toLowerCase()
-    const postBody = post.querySelector(".body").textContent.toLowerCase()
-
-    if (postTitle.includes(inputValue) || postBody.includes(inputValue)) {
-
-    }
-
-  })
-})
+addPostsIntoDOM()
+window.addEventListener("scroll", handleScrollToPageBottom)
+filterInput.addEventListener("input", handleInputValue)
